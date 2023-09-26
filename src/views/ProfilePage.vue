6c935ee852c58a02ed53659efc908e3491490823
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2023-09-26 19:54:41 -0400

    Added to ProfilePage.vue

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -2,7 +2,7 @@
     <ion-page>
         <ion-header>
             <ion-toolbar>
-                <ion-title>what is this</ion-title>
+                <ion-title>Profile</ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
@@ -19,11 +19,11 @@
             </div> -->
 
             <div class="grid grid-cols-10 h-full">
-                <div class="col-span-1 bg-red-800"> </div>
-                <div class="col-span-9 bg-yellow-800">
-                    <div class="flex justify-center items-center flex-col">
+                <div class="col-span-2 bg-red-800"> </div>
+                <div class="col-span-8 bg-yellow-800 whitespace-normal">
+                    <div class="max-w-full flex justify-center items-center flex-col mt-10 whitespace-normal">
                         <!-- Profile Image -->
-                        <div class="text-center block w-20 h-20 rounded-full">
+                        <div class="text-center block w-20 h-20 overflow-hidden rounded-full mb-5">
                             <ion-img src="https://upload.wikimedia.org/wikipedia/commons/8/8d/Frog_on_palm_frond.jpg"
                                 alt="Profile Image"></ion-img>
                         </div>
@@ -31,13 +31,39 @@
 
                         <!-- Username -->
                         <div class="text center block">
-                            <ion-label class="text-xl">USRENAME</ion-label>
+                            <ion-label class="text-3xl font-semibold">USRENAME</ion-label>
                         </div>
 
                         <!-- User's @ tag' -->
                         <div class="text center block ">
                             <ion-label class="text-lg">@usertag123</ion-label>
                         </div>
+
+                        <!-- Recent posts section -->
+                        <!-- Recent posts section -->
+                        <ion-list class="max-w-full h-screen overflow-hidden">
+                            <ion-item class="max-w-full overflow-hidden">
+                                <ion-label class="ion-text-wrap max-w-full whitespace-normal overflow-hidden">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quod, nesciunt
+                                    reiciendis laborum possimus, deserunt eius iusto qui culpa animi consequatur assumenda
+                                    incidunt dolor, non perferendis et doloremque alias odio!
+                                </ion-label>
+                            </ion-item>
+                            <ion-item class="max-w-full whitespace-normal overflow-hidden">
+                                <ion-label class="ion-text-wrap max-w-full whitespace-normal overflow-hidden">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, distinctio magni
+                                    optio cumque id doloremque porro, harum enim consectetur reprehenderit ipsa hic voluptas
+                                    blanditiis quis, nemo cupiditate ea aperiam exercitationem.
+                                </ion-label>
+                            </ion-item>
+                            <ion-item class="max-w-full whitespace-normal overflow-hidden">
+                                <ion-label class="ion-text-wrap max-w-full whitespace-normal overflow-hidden">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium minima porro neque
+                                    quis hic provident suscipit facilis, dolorum perferendis nesciunt nihil aut perspiciatis
+                                    nobis iste amet eaque recusandae illo reprehenderit.
+                                </ion-label>
+                            </ion-item>
+                        </ion-list>
                     </div>
                 </div>
             </div>
@@ -47,7 +73,7 @@
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonLabel } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonLabel, IonList, IonItem } from '@ionic/vue';
 export default {
     components: {
         IonPage,
@@ -57,6 +83,8 @@ export default {
         IonContent,
         IonImg,
         IonLabel,
+        IonList,
+        IonItem
     },
 };
 </script>
