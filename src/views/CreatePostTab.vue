4b74434c5b1674b6c74dfb16d8ff6224e95b104d
Joshua Burdick <joshburdick1@hotmail.com>
2023-10-03 14:35:14 -0400

    reworked router, changed tab icons

--- src/views/CreatePostTab.vue
+++ src/views/CreatePostTab.vue
@@ -1,23 +1,23 @@
 <template>
   <ion-page>
-    <ion-header>
+    <!-- <ion-header>
       <ion-toolbar>
         <ion-title>Tab 2</ion-title>
       </ion-toolbar>
-    </ion-header>
+    </ion-header> -->
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+      <!-- <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
-      </ion-header>
+      </ion-header> -->
 
-      <ExploreContainer name="Tab 2 page" />
+      <ion-input class="rounded border-2 border-orange-500 w-1/2 h-1/2 pl-1 py-1" placeholder="Write something..."></ion-input>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,19 +3,16 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon aria-hidden="true" :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+        <ion-tab-button tab="feed" href="/feed">
+          <ion-icon aria-hidden="true" :icon="homeOutline" />
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+        <ion-tab-button tab="create-post" href="/create_post">
+          <ion-icon aria-hidden="true" :icon="addCircleOutline" />
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon aria-hidden="true" :icon="square" />
-          <ion-label>Tab 3</ion-label>
+        <ion-tab-button tab="account" href="/account">
+          <ion-icon aria-hidden="true" :icon="personCircleOutline" />
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -24,5 +21,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { addCircleOutline, homeOutline, personCircleOutline } from 'ionicons/icons';
 </script>
