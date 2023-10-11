73a169185c48b9dddc2837e65b5093c9356aa0fd
Joshua Burdick <joshburdick1@hotmail.com>
2023-10-10 22:18:13 -0400

    removed obsolete component import

--- src/views/ProfileTab.vue
+++ src/views/ProfileTab.vue
@@ -17,5 +17,5 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+
 </script>
