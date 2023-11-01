85394fd660e235f119a894159ccfd5b9ae35b30d
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2023-11-01 15:24:19 -0400

    updated profile page
    added tailwind styling to profile page to make a better looking UI

--- src/views/ProfileTab.vue
+++ src/views/ProfileTab.vue
@@ -1,8 +1,8 @@
 <template>
     <ion-page>
-        <ion-header class="flex items-center justify-center">
-            <ion-toolbar class="flex items-center justify-center">
-                <ion-title class="flex items-center justify-center text-2xl">Profile</ion-title>
+        <ion-header class="">
+            <ion-toolbar class="">
+                <ion-title class=""></ion-title>
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
@@ -19,33 +19,18 @@
             </div> -->
 
             <!-- Divided the screen into 2 columns, sum of both columns = 100% -->
-            <div class="grid grid-cols-10 h-full">
-
-                <!-- 20% of the screen is this column, which holds the menu for a profile -->
-                <div class="col-span-2 flex items-center justify-center">
-                    <ion-list class="w-full bg-transparent">
-                        <ion-item class="cursor-pointer">
-                            <h1 class="ion-text-wrap text-lg">Posts</h1>
-                        </ion-item>
-                        <ion-item class="cursor-pointer">
-                            <h1 class="ion-text-wrap text-lg">Media</h1>
-                        </ion-item>
-                        <ion-item class="cursor-pointer">
-                            <h1 class="ion-text-wrap text-lg">Communities</h1>
-                        </ion-item>
-                    </ion-list>
-                </div>
+            <div class="h-full">
 
                 <!-- 20% of the screen is this column, which holds profile and its   -->
-                <div class="col-span-8 whitespace-normal">
+                <div class="whitespace-normal">
                     <div class="max-w-340px flex justify-center items-center flex-col mt-10 whitespace-normal">
                         <!-- Profile Image -->
                         <div class="text-center block overflow-hidden mb-5">
                             <!-- <ion-img src="https://upload.wikimedia.org/wikipedia/commons/8/8d/Frog_on_palm_frond.jpg"
                                 alt="Profile Image"></ion-img> -->
                             <ion-avatar class="cursor-pointer max-w-full">
-                                <!-- <img src="https://i.scdn.co/image/ab6761610000e5ebba025c8f62612b2ca6bfa375"
-                                    alt="Profile Image"> -->
+                                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2185d2d-96b8-43e6-89e9-0eaf5167af92/dfrv4fo-671aa9c2-49e9-491a-9550-9e4fa9575542.png/v1/fill/w_718,h_761,q_80,strp/gumball_s_funny_face_by_butterwhip2003_dfrv4fo-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzYxIiwicGF0aCI6IlwvZlwvYTIxODVkMmQtOTZiOC00M2U2LTg5ZTktMGVhZjUxNjdhZjkyXC9kZnJ2NGZvLTY3MWFhOWMyLTQ5ZTktNDkxYS05NTUwLTllNGZhOTU3NTU0Mi5wbmciLCJ3aWR0aCI6Ijw9NzE4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Sl96ojbOS_db8IT5Xe7JdxPTFJ5T5fz_9AuIvtyD18I"
+                                    alt="Profile Image">
                             </ion-avatar>
                         </div>
 
@@ -62,20 +47,36 @@
                         </div>
 
                         <!-- Communities User is involved in -->
-                        <div class="text center block mb-5">
+                        <div class="text center block mb-10">
                             <ion-label class="text-lg" style="font-size: 1.25rem;">Involved in {{ numCommunities }}
                                 communities.</ion-label>
                         </div>
 
+                        <!-- 20% of the screen is this column, which holds the menu for a profile -->
+                        <div class="flex items-center justify-between">
+                            <ul class="flex items-center justify-between w-full bg-transparent">
+                                <li class="cursor-pointer  hover:transition-opacity hover:opacity-30">
+                                    <h1 class="ion-text-wrap text-lg mx-10">Posts</h1>
+                                </li>
+                                <li class="cursor-pointer hover:transition-opacity hover:opacity-30">
+                                    <h1 class="ion-text-wrap text-lg mx-10">Media</h1>
+                                </li>
+                                <li class="cursor-pointer hover:transition-opacity hover:opacity-30">
+                                    <h1 class="ion-text-wrap text-lg mx-10">Communities</h1>
+                                </li>
+                            </ul>
+                        </div>
+
                         <!-- Recent posts section -->
-                        <ion-list class="max-w-full h-screen overflow-hidden bg-transparent">
-                            <ion-item class="" v-for="post in posts">
-                                <h1
-                                    class="sm:text-sm md:text-2
-                                    xl lg:text-3xl ion-text-wrap max-w-full whitespace-normal overflow-hidden sm:my-8 md:my-5">
-                                    {{ post.content }}
-                                </h1>
-                                <h6>{{ post.time }}</h6>
+                        <ion-list class="w-1/2 h-screen overflow-hidden bg-transparent">
+                            <ion-item class="cursor-pointer w-full" v-for="post in posts">
+                                <div
+                                    class="block sm:text-sm md:text-lg lg:text-lg ion-text-wrap max-w-full whitespace-normal overflow-hidden sm:my-8 md:my-5">
+                                    <h1>
+                                        {{ post.content }}
+                                    </h1>
+                                    <p class="text-xs mt-3">on {{ post.time }}</p>
+                                </div>
                             </ion-item>
                             <!-- <ion-item class="cursor-pointer max-w-full overflow-hidden md:my-5">
                                 <ion-label
@@ -139,9 +140,9 @@ onMounted(async () => {
     // console.log("type of postsResponse", typeof(postsResponse));
     console.log("posts", posts);
     posts.value.forEach(post => {
-        console.log(post, " has type ", typeof(post));
-        const datePosted = new Date(post.time).toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) ;
-        post.time = post.time as Date; 
+        console.log(post, " has type ", typeof (post));
+        const datePosted = new Date(post.time).toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
+        post.time = post.time as Date;
         post.time = datePosted;
     });
 });
