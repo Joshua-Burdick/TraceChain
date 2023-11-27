949dabba33a7a0f4dbd31d4fe09012bb8496efd2
eminahmetovic <ahmetovic.emin@hotmail.com>
2023-11-26 21:47:06 -0500

    updated ui

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -1,63 +1,103 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title style = "text-align: center;">TraceChain</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
-   <ion-content>
-   <div class="row">
-   <div class = "col">
-      <div class="text-center">
-        <p style = "text-align: center;">Sign up by entering your information below</p>
+      <ion-header>
+    <ion-toolbar>
+      <ion-title style = "text-align: center;">TraceChain</ion-title>
+    </ion-toolbar>
+  </ion-header>
+    <ion-content class="ion-padding">
+      <div class="createAccount-container">
+        <ion-img class="logo" src="@/assets/TraceChain.png" alt="Logo"></ion-img>
+        <ion-card class="createAccount-card">
+          <ion-card-header>
+            <ion-card-title class="create-account-text" >Create Account</ion-card-title>
+          </ion-card-header>
+          <ion-card-content>
+            <ion-list>
+              <ion-item>
+          <ion-label position="stacked">Email</ion-label>
+          <ion-input  v-model="email" name="email" autocomplete type="email"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Username</ion-label>
+          <ion-input  v-model="usertag" name="usertag" autocomplete type="usertag"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Display Name</ion-label>
+          <ion-input  v-model="username" name="username" autocomplete type="username"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label position="stacked">Password</ion-label>
+          <ion-input  v-model="password" name="password" autocomplete type="password"></ion-input>
+        </ion-item>
+        <div class="ion-text-center">
       </div>
+              </ion-list>
+              <ion-button expand="full" color="danger" class="create-account-button">Create Account</ion-button>
+          </ion-card-content>
+        </ion-card>
       </div>
-      </div>
-      <ion-list inset="true">
-        <form @submit.prevent="handleLogin">
-          <ion-item>
-            <ion-label position="stacked">Email</ion-label>
-            <ion-input v-model="email" name="email" autocomplete type="email"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label position="stacked">Username</ion-label>
-            <ion-input v-model="username" name="username" autocomplete type="username"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label position="stacked">Password</ion-label>
-            <ion-input v-model="password" name="password" autocomplete type="password"></ion-input>
-          </ion-item>
-          <div class="ion-text-center"><ion-item>
-            <ion-label position="stacked">First Name</ion-label>
-            <ion-input v-model="firstname" name="firstname" autocomplete type="firstname"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-label position="stacked">Last Name</ion-label>
-            <ion-input v-model="lastname" name="lastname" autocomplete type="lastname"></ion-input>
-          </ion-item>
-            <ion-button style = "text-align: center;" type="submit" fill="clear">Submit</ion-button>
-          </div>
-        </form>
-      </ion-list>
     </ion-content>
-
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonLabel, IonList, IonItem, IonAvatar } from '@ionic/vue';
 import { ref } from 'vue';
+import { IonPage, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonLabel, IonInput, IonButton, IonImg } from '@ionic/vue';
+import { useIonRouter } from '@ionic/vue';
+import axios from 'axios';
 
-const username = ref("initial");
-const numCommunities = ref(3);
-const email = ref("email@email.com");
-const password = ref("password");
-const firstname = ref("fname");
-const lastname = ref("lname");
+const username = ref('');
+const usertag = ref('');
+const email = ref('');
+const password = ref('');
+const router = useIonRouter();
 
-function handleLogin() {
-  // TODO - handle login
-}
 
 </script>
+
+<style scoped>
+.create-account-text{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 10px;
+  margin-top: 10px;
+}
+.createAccount-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 75vh;
+  
+}
+
+.logo {
+  width: 100px;
+  height: 100px;
+  
+  margin-bottom: 15px;
+}
+
+.createAccount-card {
+  background: radial-gradient(circle, #050505, #0d0d0d, #1a1a1a, #2a2a2a, #3b3b3b);
+color: #fff; 
+border-radius: 15px;
+width: 100%;
+width: 120%; /* Adjust the width as needed */
+max-width: 600px; /* Set a maximum width if necessary */
+margin: auto;
+}
+
+.create-account-button {
+background-color: #cf3c4f;
+color: #cf3c4f;
+border-radius: 15px;
+box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+margin-bottom: 15px;
+margin-top: 10px;
+}
+
+</style>
