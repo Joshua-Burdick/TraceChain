791e8b689aa78750b3a6c469f73dc3e42bd68b54
Joshua Burdick <joshburdick1@hotmail.com>
2023-11-30 16:37:45 -0500

    created profile header component

---
+++ src/components/Profile/ProfileHeader.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="relative">
+        <div class="absolute w-full h-full bg-gradient-to-r from-[#2f0000] via-[#790909] via-35% -inset-1 blur-sm z-0"></div>
+        <div class="flex flex-row w-full h-full z-20">
+            <!-- Profile Image -->
+            <div class="flex align-center justify-center lg:ml-7 lg:mt-7 m-3 shadow-lg shadow-black rounded-full lg:h-60 h-36 lg:w-60 w-36">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1024px-Default_pfp.svg.png"
+                    alt="Profile Image"
+                    class="rounded-full lg:h-60 h-36 lg:w-60 w-36"
+                >
+            </div>
+            <div class="flex flex-col lg:ml-2">
+                <div class="flex lg:h-14 h-10"></div>
+                <!-- Display Name -->
+                <div class="font-semibold lg:text-6xl text-2xl">
+                    {{ displayName }}
+                </div>
+                <!-- User's @ tag' -->
+                <div class="lg:text-2xl text-sm ml-2">
+                    @{{ usertag }}
+                </div>
+
+                <div class="flex flex-row text-md mt-7 ml-2">
+                    <div class="flex flex-col align-center mr-5 hover:bg-slate-700 hover:bg-opacity-70 rounded-lg p-3 cursor-pointer">
+                        <p>{{ numFollowers ? numFollowers : 0 }}</p>
+                        <p>Followers</p>
+                    </div>
+                    <div class="flex flex-col align-center mr-5 hover:bg-slate-700 hover:bg-opacity-70 rounded-lg p-3 cursor-pointer">
+                        <p>{{ numFollowing ? numFollowing : 0 }}</p>
+                        <p>Following</p>
+                    </div>
+                    <div class="flex flex-col align-center hover:bg-slate-700 hover:bg-opacity-70 rounded-lg p-3 cursor-pointer">
+                        <p>{{ numCommunities ? numCommunities : 0 }}</p>
+                        <p>Communities</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonLabel, IonList, IonItem, IonAvatar, IonIcon} from '@ionic/vue';
+
+defineProps({
+    displayName: String,
+    usertag: String,
+    numFollowers: Number,
+    numFollowing: Number,
+    numCommunities: Number
+})
+</script>
--- src/components/Profile/ProfilePage.vue
+++ src/components/Profile/ProfilePage.vue
@@ -1,33 +1,11 @@
 <template>
-    <div class="flex flex-row h-full text-slate-50 bg-[#1d1f20] overflow-scroll whitespace-normal">
+    <div class="flex flex-col">
+        <ProfileHeader :displayName="username" :usertag="usertag" :numCommunities="numCommunities" class="flex h-auto ml-1 lg:pb-5 sm:max-md:pb-1"/>
+        <div class="flex h-full"></div>
+    </div>
+    <!-- <div class="flex flex-row h-full text-slate-50 bg-[#1d1f20] overflow-scroll whitespace-normal">
         <div class="flex flex-col pt-10 align-center w-full">
-            <!-- Profile Image -->
-            <div class="text-center block mb-5">
-                <ion-avatar class="cursor-pointer max-w-full">
-                    <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a2185d2d-96b8-43e6-89e9-0eaf5167af92/dfrv4fo-671aa9c2-49e9-491a-9550-9e4fa9575542.png/v1/fill/w_718,h_761,q_80,strp/gumball_s_funny_face_by_butterwhip2003_dfrv4fo-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzYxIiwicGF0aCI6IlwvZlwvYTIxODVkMmQtOTZiOC00M2U2LTg5ZTktMGVhZjUxNjdhZjkyXC9kZnJ2NGZvLTY3MWFhOWMyLTQ5ZTktNDkxYS05NTUwLTllNGZhOTU3NTU0Mi5wbmciLCJ3aWR0aCI6Ijw9NzE4In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Sl96ojbOS_db8IT5Xe7JdxPTFJ5T5fz_9AuIvtyD18I"
-                        alt="Profile Image">
-                </ion-avatar>
-            </div>
 
-
-            <!-- Username -->
-            <div class="text center block">
-                <ion-label class="text-4xl font-semibold">{{
-                    username }}</ion-label>
-            </div>
-
-            <!-- User's @ tag' -->
-            <div class="text center block mb-3">
-                <ion-label class="text-xl">@{{ usertag }}</ion-label>
-            </div>
-
-            <!-- Communities User is involved in -->
-            <div class="text center block mb-10">
-                <ion-label class="text-lg" style="font-size: 1.25rem;">Involved in {{ numCommunities }}
-                    communities.</ion-label>
-            </div>
-
-            <!-- 20% of the screen is this column, which holds the menu for a profile -->
             <div class="flex items-center justify-between">
                 <ul class="flex items-center justify-between w-full bg-transparent">
                     <li class="cursor-pointer rounded-lg hover:bg-stone-700 active:bg-stone-800">
@@ -42,9 +20,6 @@
                 </ul>
             </div>
 
-            <br>
-
-            <!-- Recent posts section -->
             <ion-list class="w-1/2 bg-[#1d1f20]">
                 <li class="cursor-pointer w-full bg-[#1d1f20]" v-for="post in posts">
                     <div class="flex bg-stone-700 hover:bg-stone-800 active:bg-stone-900 block border-2 border-stone-400 p-3 rounded sm:text-sm md:text-lg lg:text-lg ion-text-wrap max-w-full h-full my-5 whitespace-normal overflow-hidden">
@@ -61,14 +36,14 @@
                 </li>
             </ion-list>
         </div>
-        <div class="w-1/6"></div>
-    </div>
+    </div> -->
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonLabel, IonList, IonItem, IonAvatar, IonIcon} from '@ionic/vue';
 import axios from "axios";
 import { ref, onMounted } from 'vue';
+import ProfileHeader from '@/components/Profile/ProfileHeader.vue';
 import LikesDislikes from '@/components/LikesDislikes.vue';
 
 interface Post {
