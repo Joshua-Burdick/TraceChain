111822546246116b1968c6c2aa6da8d19443dc84
Joshua Burdick <joshburdick1@hotmail.com>
2023-12-07 16:45:00 -0500

    axios url target fix

--- src/initState.ts
+++ src/initState.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export async function init() {
-  axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL || "http://localhost:1776";
+  axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL || "http://localhost:1776/api/";
 }
