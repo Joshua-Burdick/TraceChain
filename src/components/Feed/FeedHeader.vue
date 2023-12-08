48b67c1d7f8ae9112f145b191d5854d8d484f8be
Joshua Burdick <joshburdick1@hotmail.com>
2023-12-08 12:09:32 -0500

    Feed Page header, app text color change

---
+++ src/components/Feed/FeedHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-col w-full h-full text-slate-100 align-center">
+        <div v-if="$vuetify.display.mdAndDown" class="flex relative w-full h-full align-center py-5">
+            <div class="absolute w-full h-full bg-stone-800 bg-opacity-70 -inset-1 blur-sm z-0"></div>
+            <div class="flex flex-row w-full h-full align-center justify-center z-20">
+                <img aria-hidden="true" src="/TraceChain.svg" class="w-20"/>
+                <p class="text-4xl font-semibold">TraceChain</p>
+            </div>
+        </div>
+        <div class="flex flex-row w-full h-full py-7 justify-center bg-stone-800 bg-opacity-50 text-8xl font-semibold">
+            For You
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonImg, IonLabel, IonList, IonItem, IonAvatar, IonIcon} from '@ionic/vue';
+
+defineProps({
+    displayName: String,
+    usertag: String,
+    numFollowers: Number,
+    numFollowing: Number,
+    numCommunities: Number
+})
+</script>
--- src/views/FeedTab.vue
+++ src/views/FeedTab.vue
@@ -9,6 +9,6 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar } from '@ionic/vue';
 import { ref, onMounted } from 'vue';
-import FeedPage from '../components/FeedPage.vue';
+import FeedPage from '../components/Feed/FeedPage.vue';
 import NavigationWrapper from '@/components/Navigation/NavigationWrapper.vue';
 </script>
