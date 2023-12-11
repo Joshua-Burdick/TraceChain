df93eea8634545a03a24db6d34b29ef6c34b6bd7
Joshua Burdick <joshburdick1@hotmail.com>
2023-12-10 20:21:30 -0500

    feed page header

--- src/components/Feed/FeedHeader.vue
+++ src/components/Feed/FeedHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col w-full h-full text-slate-100 align-center">
         <div v-if="$vuetify.display.mdAndDown" class="flex relative w-full h-full align-center py-5">
-            <div class="absolute w-full h-full bg-stone-800 bg-opacity-70 -inset-1 blur-sm z-0"></div>
+            <div class="absolute w-full h-full bg-stone-800 bg-opacity-70 blur-sm z-0"></div>
             <div class="flex flex-row w-full h-full align-center justify-center z-20">
                 <img aria-hidden="true" src="/TraceChain.svg" class="w-20"/>
                 <p class="text-4xl font-semibold">TraceChain</p>
