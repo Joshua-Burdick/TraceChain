f10739809a72ad37d413af48b4085da0f7339042
Joshua Burdick <joshburdick1@hotmail.com>
2023-12-10 22:46:26 -0500

    removed duplicate template

--- src/components/Post/UserPostListEditable.vue
+++ src/components/Post/UserPostListEditable.vue
@@ -14,17 +14,6 @@
             <v-progress-circular color="blue-lighten-3" class="flex w-1/2 h-1/2 justify-center items-center" :width="15" indeterminate></v-progress-circular>
         </div>
     </div>
-</template><template>
-    <div class="flex flex-row h-full w-full justify-center">
-        <ion-list v-if="!loading" class="flex flex-col w-1/2 bg-[#1d1f20]">
-            <li class="flex w-full mb-5" v-for="post in posts" :key="post.id">
-                <PostWidget :post="post" class="flex w-full"/>
-            </li>
-        </ion-list>
-        <div v-if="loading" class="flex flex-row w-full h-full justify-center items-center overflow-hidden">
-            <v-progress-circular color="blue-lighten-3" class="flex w-1/2 h-1/2 justify-center items-center" :width="15" indeterminate></v-progress-circular>
-        </div>
-    </div>
 </template>
 
 <script setup lang="ts">
