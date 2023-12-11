c48cf64ac5a0b6acdee39b78d45521a340134c33
amraibra <92942609+amraibra@users.noreply.github.com>
2023-12-11 16:27:44 -0500

    Menu + Page Styling
    Fixed the menu styling, created whole new styling for account page and added the new menus to appearance, privacy, and account (all done in tailwind)

--- src/views/Appearance.vue
+++ src/views/Appearance.vue
@@ -2,51 +2,63 @@
     <ion-page>
         <ion-header>
                 <ion-toolbar>
-                    <ion-title style="padding-left: 79vh;">Appearance Settings</ion-title>
+                    <ion-title></ion-title>
                     <ion-buttons slot="start">
                         <ion-menu-button></ion-menu-button>
+                    </ion-buttons>
+                    <ion-buttons slot="end">
+                        <ion-button @click="$router.push('/feed')">
+                            <ion-icon slot="icon-only" :icon="homeOutline"></ion-icon>
+                        </ion-button>
                     </ion-buttons>
                 </ion-toolbar>
         </ion-header>
 
+        <ion-content :fullscreen="true" class="bg-[#141313]">
+            <div class="flex flex-col lg:w-full h-full mr-10">
+                <ComingSoon name="Appearance Settings" />
+            </div>
+        </ion-content>
         <ion-menu content-id="main-content" side="start">
             <ion-content>
-                <div class="list-container">
                 <ion-list lines="none">
-                    <ion-list-header style="font-size: 1.5em;  padding-left: 40px; font-weight: 550;">
-                        Settings
-                    </ion-list-header>
                     <ion-menu-toggle auto-hide="false">
-                            <ion-item @click="$router.push('/settings')" @mouseover="highlightItem" @mouseleave="unhighlightItem">
-                                <ion-icon slot="start" :icon="personCircleOutline" style="margin-right: 12px;"></ion-icon>
-                                <ion-label>Account</ion-label>
-                            </ion-item>
-                                <ion-item @click="$router.push('/settings/appearance')">
-                                    <ion-icon slot="start" :icon="sunnyOutline" style="margin-right: 12px;"></ion-icon>
-                                    <ion-label>Appearance</ion-label>
-                                </ion-item>
-                                <ion-item @click="$router.push('/settings/privacy')">
-                                    <ion-icon slot="start" :icon="lockClosedOutline" style="margin-right: 12px;"></ion-icon>
-                                    <ion-label>Privacy</ion-label>
-                                </ion-item>
-                            <ion-item>
-                                <ion-icon slot="start" :icon="logOutOutline" style="margin-right: 12px;"></ion-icon>
-                                <ion-label>Log Out</ion-label>
-                            </ion-item>
+                        <h1 class="font-bold text-2xl ml-10 pt-[190px] mb-3">Settings</h1>
+                        <ion-item @click="$router.push('/settings')" class="flex items-center p-3 hover:bg-neutral-700
+                        rounded"
+                        style="--background: transparent; --background-hover: bg-neutral-700; --ripple-color: transparent;">
+                            <ion-icon slot="start" :icon="personCircleOutline" class="text-lg mr-3 ml-4"></ion-icon>
+                            <ion-label>Account</ion-label>
+                        </ion-item>
+                        <ion-item @click="$router.push('/settings/appearance')" class="flex items-center p-3 hover:bg-neutral-700 rounded"
+                        style="--background: transparent; --background-hover: bg-neutral-700; --ripple-color: transparent;">
+                            <ion-icon slot="start" :icon="sunnyOutline" class="text-lg mr-3 ml-4"></ion-icon>
+                            <ion-label>Appearance</ion-label>
+                        </ion-item>
+                        <ion-item @click="$router.push('/settings/privacy')" class="flex items-center p-3 hover:bg-neutral-700 rounded" 
+                        style="--background: transparent; --background-hover: bg-neutral-700; --ripple-color: transparent;">
+                            <ion-icon slot="start" :icon="lockClosedOutline" class="text-lg mr-3 ml-4"></ion-icon>
+                            <ion-label>Privacy</ion-label>
+                        </ion-item>
+                        <ion-item @click="$router.push('/logout')" class="flex items-center p-3 hover:bg-neutral-700 rounded pb-"
+                        style="--background: transparent; --background-hover: bg-neutral-700; --ripple-color: transparent;">
+                            <ion-icon slot="start" :icon="logOutOutline" class="text-lg mr-3 ml-4"></ion-icon>
+                            <ion-label>Log Out</ion-label>
+                        </ion-item>
+                        <div class="pt-20"></div>
+                        <div class="pt-20"></div>
+                        <div class="pt-5"></div>
                     </ion-menu-toggle>
                 </ion-list>
-            </div>
             </ion-content>
         </ion-menu>
-
-        <ion-content id="main-content">
-        </ion-content>
     </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonTitle, IonHeader, IonToolbar, IonContent, IonMenu } from '@ionic/vue';
-import { lockClosedOutline, personCircleOutline, sunnyOutline, logOutOutline } from 'ionicons/icons';
+import { lockClosedOutline, personCircleOutline, sunnyOutline, logOutOutline, homeOutline } from 'ionicons/icons';
+import ComingSoon from '../components/ComingSoon.vue';
 
 const highlightItem = (event: MouseEvent) => {
 (event.target as HTMLElement).classList.add('item-highlight');
@@ -57,75 +69,3 @@ const unhighlightItem = (event: MouseEvent) => {
 };
 
 </script>
-
-<style>
-
-ion-menu {
---width: 200px;
---min-width: 300px;
-}
-
-ion-menu {
-height: 100vh;
-}
-
-ion-menu ion-content {
-height: 100%;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-  --background: #1e1e1e;
-  --padding-top: 0;        /* Removes default padding */
-  --padding-bottom: 0;     /* Removes default padding */
-  --padding-start: 0;      /* Removes default padding */
-  --padding-end: 0;
-}
-
-.list-container {
-height: 100%;
-padding-top: 55%;
-}
-
-ion-list ion-item {
-padding-bottom: 30px;
-}
-
-
-ion-item {
---background-activated: transparent; /* Background when activated */
---background-hover: transparent; /* Background for hover state */
---background-hover: transparent; /* No background on hover */
---ripple-color: transparent;
---highlight-color-focused: white;
---placeholder-color: white;
---color: white;
-}
-
-/* For browsers that support hover states, you can also use the :hover pseudo-class */
-ion-item:hover::part(native) {
-background-color: #1c1c1c; /* Your desired hover color */
-}
-
-ion-item::part(native):hover {
-background-image: linear-gradient(to right, #632323, transparent);
-}
-
-ion-list {
-height: 100%;
---background: tranparent;
-}
-
-ion-list-header {
-padding-bottom: 30px;
-}
-
-ion-menu-toggle {
---background: transparent;
-}
-
-ion-icon {
-padding-left: 20px;
-}
-
-</style>
