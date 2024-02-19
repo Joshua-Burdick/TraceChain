355bbca7e1058ec104133784e7ba0dd69ab87179
Joshua Burdick <joshburdick1@hotmail.com>
2024-02-19 18:50:44 -0500

    post context menu delete dialog

--- src/components/Post/PostContextMenu.vue
+++ src/components/Post/PostContextMenu.vue
@@ -6,7 +6,7 @@
             class="text-2xl hover:bg-slate-500 hover:bg-opacity-70 active:bg-slate-700 active:bg-opacity-80 rounded-lg p-1 cursor-pointer"
         />
         <v-menu activator="parent">
-            <v-list style="background-color: rgb(214 211 209);">
+            <v-list style="background-color: rgb(168, 167, 167);">
                 <v-list-item
                     v-for="(item, index) in actions"
                     :key="index"
@@ -14,7 +14,7 @@
                     class="mx-2 rounded-lg"
                     :class="{
                         'hover:bg-stone-400' : item.title !== 'Delete',
-                        'hover:bg-red-600' : item.title === 'Delete'
+                        'hover:bg-red-700' : item.title === 'Delete'
                     }"
                     @click.stop="item.action()"
                 >
@@ -27,9 +27,13 @@
                 </v-list-item>
             </v-list>
         </v-menu>
-        <v-dialog v-model="dialog" class="w-1/3">
-            <div class="flex bg-red-500 w-full justify-center">
-                This is a dialog box
+        <v-dialog v-model="deleteDialog" class="w-1/3">
+            <div class="flex flex-col bg-[#1d1f20] text-slate-100 rounded-lg border-2 border-slate-700 p-5 w-full justify-center">
+                <p class="text-xl">Are you sure you want to delete this post? This action cannot be undone.</p>
+                <div class="flex flex-row justify-end p-3">
+                    <button class="hover:bg-gray-500 active:bg-gray-600 rounded-lg p-2 mr-2" @click.stop="deleteDialog = false">Cancel</button>
+                    <button class="bg-red-700 active:bg-red-800 rounded-lg p-2" @click.stop="deletePost">Delete</button>
+                </div>
             </div>
         </v-dialog>
     </div>
@@ -38,37 +42,36 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonToggle, IonIcon, IonItem } from '@ionic/vue';
 import { ellipsisVerticalOutline, pencilSharp, trash } from 'ionicons/icons';
-import { onMounted, ref, Ref, PropType, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { ref, Ref } from 'vue';
 import axios from 'axios';
 
 const actions = [
     {
         title: "Edit",
         action: () => {
-            console.log("Edit");
-            dialog.value = true;
+            editDialog.value = true;
         },
         icon: pencilSharp
     },
     {
         title: "Delete",
         action: () => {
-            console.log("Delete");
-            dialog.value = true;
+            deleteDialog.value = true;
         },
         icon: trash
     }
 ]
 
-const contextMenu = ref(false);
-const dialog = ref(false);
+const editDialog: Ref<boolean> = ref(false);
+const deleteDialog: Ref<boolean> = ref(false);
 
 const props = defineProps({
     postId: String
 });
 
-watch(contextMenu, (value) => {
-    console.log("contextMenu", value);
-});
+const deletePost = async () => {
+    await axios.delete(`post/${props.postId}`)
+        .catch((err) => console.error(err));
+    deleteDialog.value = false;
+}
 </script>
