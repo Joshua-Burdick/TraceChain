3f327df07e592f931881428a8d498c34eeb22e01
Joshua Burdick <joshburdick1@hotmail.com>
2024-02-19 20:32:34 -0500

    v-model type

--- src/components/Post/EditPost.vue
+++ src/components/Post/EditPost.vue
@@ -14,7 +14,7 @@
         <div class="flex flex-col w-full h-auto pt-7 justify-center align-center">
             <p class="text-4xl font-semibold pb-3">Sources</p>
             <label class="relative inline-flex items-center cursor-pointer">
-                <input type="checkbox" value="" class="sr-only peer" v-model="post.isInformative">
+                <input type="checkbox" value="" class="sr-only peer" v-model="(post.isInformative as any)">
                 <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                 <span class="ms-3 text-lg text-gray-900 dark:text-gray-300">Is this post informative?</span>
             </label>
