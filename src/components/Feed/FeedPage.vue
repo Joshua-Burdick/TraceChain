40bdd0fb2bf50f659184cb03638f3a26024c76dd
amraibra <92942609+amraibra@users.noreply.github.com>
2024-03-12 13:41:06 -0400

    Sort by most popular, oldest, and newest complete

--- src/components/Feed/FeedPage.vue
+++ src/components/Feed/FeedPage.vue
@@ -4,8 +4,8 @@
         <div class="flex justify-center items-center pt-10 relative">
 
             <!--Feed List-->
-            <ion-list v-if="!loading" class="flex flex-col w-1/2 bg-[#1d1f20]">
-                <li class="flex w-full mb-5" v-for="post in feed" :key="post._id">
+            <ion-list v-if="!loading" class="flex flex-col w-1/2 bg-[#1d1f20]" :key="sortType">
+                <li class="flex w-full mb-5" v-for="post in sortedFeed" :key="post._id">
                     <PostWidget :post="post" class="flex w-full" :variant="'feed'"/>
                 </li>
             </ion-list>
@@ -16,9 +16,9 @@
                   Sort by
               </button>
               <div v-if="open" class="absolute right-0 mt-2 w-40 rounded-md shadow-lg z-10 bg-neutral-700">
-                  <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" href="#">Newest</a>
-                  <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" href="#">Oldest</a>
-                  <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" href="#">Most Popular</a>
+                  <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" @click.prevent="sortType='Newest'">Newest</a>
+                  <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" @click.prevent="sortType='Oldest'">Oldest</a>
+                  <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" @click="sortType = 'Most Popular'">Most Popular</a>
                   <a class="block px-4 py-2 text-sm text-white hover:bg-neutral-600" href="#">Following</a>
               </div>
           </div>
@@ -32,12 +32,13 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonList } from '@ionic/vue';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed, watch } from 'vue';
 import axios from 'axios';
 import FeedHeader from '@/components/Feed/FeedHeader.vue';
 import PostWidget from '@/components/Post/PostWidget.vue';
 
 const open = ref(false);
+const sortType = ref(localStorage.getItem('sortType') || 'Newest');
 
 function toggleDropdown() {
     open.value = !open.value;
@@ -68,6 +69,7 @@ onMounted(async () => {
     try {
         const feedResponse = await axios.get("/post/feed");
         feed.value = feedResponse.data;
+        console.log('First post:', feed.value[0]);
     } catch (error) {
         console.error('Error fetching feed:', error);
     }
@@ -82,5 +84,23 @@ const formatSource = (source: any) => {
         return 'http://' + source;
     }
 }
+
+const sortedFeed = computed(() => {
+    console.log('Sorting by:', sortType.value);
+    switch (sortType.value) {
+        case 'Newest':
+            return [...feed.value].sort((a, b) => new Date(b.time).getTime() - new Date(a.time).getTime());
+        case 'Oldest':
+            return [...feed.value].sort((a, b) => new Date(a.time).getTime() - new Date(b.time).getTime());
+        case 'Most Popular':
+            return [...feed.value].sort((a, b) => b.likes.length - a.likes.length);
+        default:
+            return feed.value;
+    }
+});
+
+watch(sortType, (newValue) => {
+    localStorage.setItem('sortType', newValue);
+});
 
 </script>
