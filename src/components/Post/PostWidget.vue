9a1ddbbc5a46532912f99b3cc1db82df0f073e24
Joshua Burdick <joshburdick1@hotmail.com>
2024-03-26 14:27:49 -0400

    quote post preview

--- src/components/Post/PostWidget.vue
+++ src/components/Post/PostWidget.vue
@@ -7,7 +7,7 @@
             <div v-else-if="!post.isInformative" class="flex flex-row w-[10px] rounded-lg mr-3 bg-gradient-to-b from-[#800000] via-[#9f0a0a] via-35% to-[#b00700]"></div>
             <div class="flex flex-col items-start w-full">
                 <div
-                    @click.stop="$router.push(`/post/${post._id}`)"
+                    @click.stop="$router.push(`/post/${post._id}`)" 
                 >
                     <div>
                         <span
@@ -23,6 +23,27 @@
                     </div>
                     <div v-else class="text-2xl px-2 py-3 mb-2">
                         {{ post.content.substring(0,200) + "..." }} <p class="text-sm text-blue-400 hover:underline">Read More</p>
+                    </div>
+                </div>
+                <div
+                    v-if="post.parentPostId"
+                    class="flex flex-col border-2 border-stone-400 bg-stone-600 hover:bg-stone-500 rounded-lg p-2 mx-8 mb-2 w-3/4"
+                    @click="$router.push(`/post/${parentPost._id}`)"
+                >
+                    <div>
+                        <span
+                            class="text-md hover:underline"
+                            @click.stop="$router.push({ path: `/profile/${parentPost.userId}` })"
+                        >
+                            {{ `${parentPostMeta.username}@${parentPostMeta.usertag}` }}
+                        </span>
+                        <span class="text-xs text-slate-300">{{ parentPost.isEdited ? ' Edited' : ' Posted' }} @{{ timeString }} on {{ dateString }}</span>
+                    </div>
+                    <div v-if="parentPost.content.length < 200" class="text-2xl px-2 py-3 mb-2">
+                    {{ parentPost.content }}
+                    </div>
+                    <div v-else class="text-2xl px-2 py-3 mb-2">
+                        {{ parentPost.content.substring(0,200) + "..." }} <p class="text-sm text-blue-400 hover:underline">Read More</p>
                     </div>
                 </div>
                 <div
@@ -75,7 +96,7 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonToggle, IonIcon, IonItem } from '@ionic/vue';
 import { thumbsDownSharp, thumbsUpSharp, chatbubbleEllipsesOutline } from 'ionicons/icons';
-import { onMounted, ref, PropType } from 'vue';
+import { onMounted, ref, Ref, PropType } from 'vue';
 import { useRoute } from 'vue-router';
 import axios from 'axios';
 
@@ -97,6 +118,7 @@ interface Post {
     replies: [String],
     isInformative: Boolean,
     isEdited: Boolean,
+    parentPostId: String,
     likes: [String],
     dislikes: [String]
 }
@@ -108,6 +130,8 @@ const timeString = ref("");
 const username = ref("");
 const usertag = ref("");
 const userId = sessionStorage.getItem("userId") ?? localStorage.getItem("userId");
+const parentPost: Ref<Post> = ref({} as Post);
+const parentPostMeta: Ref<{username: string, usertag: string}> = ref({username: "", usertag: ""});
 
 const props = defineProps({
     post: {
@@ -134,6 +158,39 @@ onMounted(async () => {
         });
     username.value = postHeader.username;
     usertag.value = postHeader.usertag;
+
+    if (props.post.parentPostId) {
+        const parentPostResponse = await axios.get(`post/${props.post.parentPostId}`)
+            .then((res) => res.data)
+            .catch((err) => {
+                return {
+                    _id: "",
+                    userId: "",
+                    time: new Date(),
+                    content: "",
+                    sources: [],
+                    replies: [],
+                    isInformative: false,
+                    isEdited: false,
+                    parentPostId: "",
+                    likes: [],
+                    dislikes: []
+                }
+            });
+            
+        parentPost.value = parentPostResponse;
+
+        const parentPostHeader = await axios.get(`account/${parentPost.value.userId}/header`)
+            .then((res) => res.data)
+            .catch((err) => {
+                return {
+                    username: "deleted",
+                    usertag: "deleted-user"
+                }
+            });
+        parentPostMeta.value.username = parentPostHeader.username;
+        parentPostMeta.value.usertag = parentPostHeader.usertag;
+    }
 });
 
 const updateLikesDislikes = async (type: string) => {
