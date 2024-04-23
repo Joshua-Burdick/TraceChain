7f92ae57c9086132ceda3994ece545996d659500
amraibra <amra.ibrahimovic@snhu.edu>
2024-04-23 15:40:55 -0400

    text-edits

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,12 +1,11 @@
 <template>
-  <ion-page>
-    <ion-content class="ion-padding">
+  <ion-page class="bg-neutral-900">
       <div class="flex flex-col items-center justify-center min-h-[75vh]">
         <div class="flex items-center justify-center mt-5 mr-12">
           <ion-img class="w-32 mt-5" src="/TraceChain.svg" alt="Logo"></ion-img>
-          <span class="text-5xl font-bold -ml-2">TraceChain</span>
+          <span class="text-5xl font-bold -ml-2 text-white">TraceChain</span>
         </div>
-        <ion-card class="bg-gradient-to-br from-neutral-900 to-neutral-700 text-white rounded-xl w-full max-w-lg m-auto p-5">
+        <ion-card class="bg-neutral-900 bg-gradient-to-br from-neutral-900 to-neutral-700 text-white rounded-xl w-full max-w-lg m-auto p-5">
           <ion-card-header class="flex flex-col items-center justify-center mb-2 mt-2">
             <ion-card-title class="text-2xl text-white">Sign In</ion-card-title>
           </ion-card-header>
@@ -56,7 +55,6 @@
         :message="alertMessage"
         :buttons="[{ text: 'OK', handler: () => showAlert = false }]"
       />
-    </ion-content>
   </ion-page>
 </template>
 
